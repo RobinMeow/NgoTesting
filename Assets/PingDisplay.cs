9eecdcdf88eebf1b93867613d0c9f9a716bd9eef
Robin <robinmeow97@gmail.com>
2023-01-04 14:32:08 +0100

    fix ping display
    had to add the networkobject to the NetworkPrefab List

--- Assets/PingDisplay.cs
+++ Assets/PingDisplay.cs
@@ -2,13 +2,14 @@ using System;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public sealed class PingDisplay : NetworkBehaviour
 {
     [SerializeField] float _refreshRateInSeconds = 1.0f;
-    TextMeshProUGUI _tmpPing;
+    [SerializeField] TextMeshProUGUI _tmpPing;
     readonly NetworkVariable<double> _ping = new NetworkVariable<double>(
-        default,
+        -1.0f,
         NetworkVariableReadPermission.Owner,
         NetworkVariableWritePermission.Server
         );
@@ -17,7 +18,7 @@ public sealed class PingDisplay : NetworkBehaviour
 
     void Awake()
     {
-        _tmpPing = GetComponent<TextMeshProUGUI>();
+        Assert.IsNotNull(_tmpPing, $"{nameof(_tmpPing)} may not be null.");
         SetDefaultDisplay();
     }
 
@@ -30,7 +31,7 @@ public sealed class PingDisplay : NetworkBehaviour
     {
         if (IsClient)
         {
-            PingServerRpc(DateTime.Now.Ticks);
+            PingServerRpc(DateTime.Now.Ticks); // ToDo: use OnValueChange and refresh UI in there. And use time to send RPCs
 
             _timePassed += Time.deltaTime;
             if (_timePassed > _refreshRateInSeconds)
