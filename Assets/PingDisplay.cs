7fc8835994c18e3225aa019c0c6e0a73dcf96317
Robin <robinmeow97@gmail.com>
2023-01-04 16:35:01 +0100

    2d2

--- Assets/PingDisplay.cs
+++ Assets/PingDisplay.cs
@@ -1,20 +1,42 @@
 using System;
+using System.Collections;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Assertions;
 
+public struct ClientPing : INetworkSerializeByMemcpy, IEquatable<ClientPing>
+{
+    public ulong ClientId;
+    public double Ping;
+    
+    public ClientPing(ulong clientId, double ping)
+    {
+        ClientId = clientId;
+        Ping = ping;
+    }
+
+    public bool Equals(ClientPing other)
+    {
+        return ClientId == other.ClientId;
+    }
+}
+
+// Should only be a single intance in scene
 public sealed class PingDisplay : NetworkBehaviour
 {
     [SerializeField] float _refreshRateInSeconds = 1.0f;
     [SerializeField] TextMeshProUGUI _tmpPing;
-    readonly NetworkVariable<double> _ping = new NetworkVariable<double>(
-        -1.0f,
+    // NetworkVariable<double> _ping = new NetworkVariable<double>(
+    //     -1.0f,
+    //     NetworkVariableReadPermission.Owner,
+    //     NetworkVariableWritePermission.Server
+    // );
+    NetworkList<ClientPing> _clientPings = new NetworkList<ClientPing>(
+        default,
         NetworkVariableReadPermission.Owner,
         NetworkVariableWritePermission.Server
-        );
-
-    float _timePassed = 0.0f;
+    );
 
     void Awake()
     {
@@ -27,34 +49,76 @@ public sealed class PingDisplay : NetworkBehaviour
         _tmpPing.text = "00ms (offline)";
     }
 
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        if (IsClient)
+        if (!IsClient)
         {
-            PingServerRpc(DateTime.Now.Ticks); // ToDo: use OnValueChange and refresh UI in there. And use time to send RPCs
-
-            _timePassed += Time.deltaTime;
-            if (_timePassed > _refreshRateInSeconds)
-            {
-                Refresh(_ping.Value);
-                _timePassed = 0.0f;
-            }
+            Debug.LogError("This should never happn, it is for future use, when I implement standalone server.");
+            _tmpPing.text = "00ms (server)";
+            enabled = false;
+            return;
         }
+        
+        WaitForSeconds refreshRate = new WaitForSeconds(_refreshRateInSeconds);
+        RegisterClientPingServerRpc(DateTime.Now.Ticks);
+        StartCoroutine(SendPings(refreshRate));
+        _clientPings.OnListChanged += OnClientPingChanged;
     }
 
-    [ServerRpc(Delivery = RpcDelivery.Unreliable, RequireOwnership = false)]
-    void PingServerRpc(long ticks)
+    [ServerRpc(RequireOwnership = false)]
+    void RegisterClientPingServerRpc(long ticks, ServerRpcParams rpcPara = default)
     {
-        _ping.Value = (DateTime.Now - new DateTime(ticks)).TotalMilliseconds;
+        if (_clientPings == null)
+        {
+            _clientPings = new NetworkList<ClientPing>(default, );
+        }
+        double ping = (DateTime.Now - new DateTime(ticks)).TotalMilliseconds;
+        ClientPing clientPing = new ClientPing(rpcPara.Receive.SenderClientId, ping);
+        _clientPings.Add(clientPing);
     }
 
-    void Refresh(double ping)
+    void OnClientPingChanged(NetworkListEvent<ClientPing> changeEvent)
+    {
+        if (changeEvent.Type == NetworkListEvent<ClientPing>.EventType.Value)
+        {
+            double ping = _clientPings[changeEvent.Index].Ping;
+            SetPingText(ping);
+        } 
+    }
+
+    IEnumerator SendPings(WaitForSeconds refreshRate)
+    {
+        while (NetworkObject.IsSpawned)
+        {
+            Debug.Log($"Ping request sent to server, by: {OwnerClientId}");
+            PingServerRpc(DateTime.Now.Ticks); 
+            yield return refreshRate;
+        }
+        
+        Debug.Log($"Ping is no longer refreshing because '{name}' despawned.");
+        SetDefaultDisplay();
+    }
+            
+    void SetPingText(double ping)
     {
         _tmpPing.text = $"{ping:00}ms";
     }
 
-    public override void OnNetworkDespawn()
+    [ServerRpc(Delivery = RpcDelivery.Unreliable, RequireOwnership = false)]
+    void PingServerRpc(long ticks, ServerRpcParams rpcPara = default)
     {
-        SetDefaultDisplay();
+        ulong senderClientId = rpcPara.Receive.SenderClientId;
+        double ping = (DateTime.Now - new DateTime(ticks)).TotalMilliseconds;
+        for (byte i = 0; i < _clientPings.Count; i++)
+        {
+            ClientPing clientPing = _clientPings[i];
+            if (clientPing.ClientId == senderClientId)
+            {
+                clientPing.Ping = ping; // can tets it, if this one raises the on changed event, than the next line isnt needed.
+                _clientPings[i] = clientPing; // idk if this is necessary. but its a struct, after all.
+                Debug.Log($"Ping ({ping}) updated by server, requested by: {senderClientId}");
+                return;
+            }            
+        }
     }
 }
